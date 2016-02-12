2c0d295145c66a79756928af050bab880a6115ea
Matthew Mahnke <mahnkem@msoe.edu>
2016-02-12 00:24:37 -0600

    Add some frivolous pseudo update pseudo version information

--- updates/mkver.sh
+++ updates/mkver.sh
@@ -8,15 +8,13 @@ for major in $(seq $MAJOR_VERS); do
   for minor in $(seq $MINOR_VERS); do
     for patch in $(seq $PATCH_VERS); do
       mkdir -p "$major/$minor/$patch"
-      file=<<EOS
+      file="
       publisher=Distributed Interactive Mobile Gaming, Inc.
       pulishDate=2016-02-29
       version=$major.$minor.$patch
       size=4000
       minFrmeworkVer=1.0.1
-      download=/download?major=$major&minor=$minor&patch=$patch
-      EOS
+      download=/download?major=$major&minor=$minor&patch=$patch"
     done
   done
 done
-

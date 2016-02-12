540537c9813a142de4cbe4837218e2ef4ac5fba9
Matthew Mahnke <mahnkem@msoe.edu>
2016-02-11 22:54:29 -0600

    Add some more stuff

--- app.rb
+++ app.rb
@@ -1,12 +1,49 @@
 #!/usr/bin/env ruby
 
 require 'sinatra'
+require 'sinatra/jsonp'
 
+
+# GET /
 get '/' do
   "Welcome! You've reached the base endpoint of this terse API"
 end
 
+
+# GET /download
+#
+# Serve the update file
+get '/download' do
+  major = params[:major]
+  minor = params[:minor]
+  patch = params[:patch]
+  'You are at the endpoint for downloading an update\n' <<
+    "You are trying to download #{major}.#{minor}.#{patch}"
+end
+
+
+# GET /update
+#
+# Reply with the JSON object of the most up-to-date version available.
+#
+# Example JSON response:
+#   {
+#     "publisher": "Distributed Interactive Mobile Gaming, Inc.",
+#     "publishDate": "2016-02-29",
+#     "version": "0.4.2",
+#     "size": 732087,
+#     "minFrameworkVer": "1.0.1",
+#     "download": "https://dimg.com/download?major=0&minor=4&patch=2"
+#   }
 get '/update' do
-  'Hello World!'
+  json_resp = {
+      publisher: 'Distributed Interactive Mobile Gaming, Inc.',
+      publishDate: Time.now,
+      version: '0.50.1',
+      size: File.size('README.md'),
+      minFrameworkVer: '1.0.5',
+      download: 'https://dimg.com/download?major=0&minor=50&patch=1'
+  }
+  JSONP json_resp
 end
 
---
+++ updates/mkver.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+readonly MAJOR_VERS=$1
+readonly MINOR_VERS=$2
+readonly PATCH_VERS=$3
+
+for major in $(seq $MAJOR_VERS); do
+  for minor in $(seq $MINOR_VERS); do
+    for patch in $(seq $PATCH_VERS); do
+      mkdir -p "$major/$minor/$patch"
+      file=<<EOS
+      publisher=Distributed Interactive Mobile Gaming, Inc.
+      pulishDate=2016-02-29
+      version=$major.$minor.$patch
+      size=4000
+      minFrmeworkVer=1.0.1
+      download=/download?major=$major&minor=$minor&patch=$patch
+      EOS
+    done
+  done
+done
+
